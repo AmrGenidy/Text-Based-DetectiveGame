27028a09850f7b5ed67fd03f30fe8627c5b89629
AmrGenidy <amrogenidy@gmail.com>
2025-03-16 19:05:15 +0300

    "testing commit"

--- src/Main/DetectiveGameMain.java
+++ src/Main/DetectiveGameMain.java
@@ -18,7 +18,7 @@ import SuspectClasses.VictorBlackwood;
 
 import java.util.*;
 
-//for the commit
+//for the pull request
 
 public class DetectiveGameMain {
     public static void main(String[] args) {
